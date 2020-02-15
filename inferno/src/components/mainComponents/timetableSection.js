048a6de8b4814f9e19f226708e0090ff12c32ca8
Caoimhe <caoimhegmalone@gmail.com>
2020-02-15 12:42:47 +0100

    timetable basic code added

--- inferno/src/components/mainComponents/timetableSection.js
+++ inferno/src/components/mainComponents/timetableSection.js
@@ -16,11 +16,13 @@ function Timetable() {
       </div>
       <div className="classesRight">
          <ul>
-            <h2>Santry</h2>
+            <h2 className="santry-heading">Santry</h2>
+            <h2 className="killester-heading">Killester</h2>
             <li>
                <div className="left-container">
                   <div className="day">Monday</div>
-                  <div className="time">6:00am, 10:00am, 6:30pm, 7:30pm</div>
+                  <div className="time santry">6:00am, 10:00am, 6:30pm, 7:30pm</div>
+                   <div className="time killester">1:00pm, 6:00pm</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
@@ -33,7 +35,8 @@ function Timetable() {
             <li>
                <div className="left-container">
                   <div className="day">Tuesday</div>
-                  <div className="time">1:00pm</div>
+                  <div className="time santry">1:00pm</div>
+                  <div className="time killester">6:00am, 10:00am, 6:30pm, 7:30pm</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
@@ -46,7 +49,8 @@ function Timetable() {
             <li>
                <div className="left-container">
                   <div className="day">Wednesday</div>
-                  <div className="time">6:00am, 10:00am</div>
+                  <div className="time santry">6:00am, 10:00am</div>
+                  <div className="time killester">1:00pm</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
@@ -59,7 +63,8 @@ function Timetable() {
             <li>
                <div className="left-container">
                   <div className="day">Thursday</div>
-                  <div className="time">1:00pm</div>
+                  <div className="time santry">1:00pm</div>
+                  <div className="time killester">6:00am, 10:00am, 6:30pm, 7:30pm</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
@@ -71,7 +76,8 @@ function Timetable() {
             <li>
                <div className="left-container">
                   <div className="day">Friday</div>
-                  <div className="time">6:00am, 10:00am, 6:30pm, 7:30pm</div>
+                  <div className="time santry">6:00am, 10:00am, 6:30pm, 7:30pm</div>
+                  <div className="time killester">1:00pm, 6:00pm</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
@@ -82,8 +88,22 @@ function Timetable() {
 
             <li>
                <div className="left-container">
-                  <div className="day">Saturday & Sunday</div>
-                  <div className="time">9:00am, 10:00am</div>
+                  <div className="day">Saturday</div>
+                  <div className="time santry">9:00am, 10:00am</div>
+                  <div className="time killester">8:00am, 9:00am</div>
+               </div>
+               <div className="right-container">
+                  <div className="book-btn">
+                     <a href="https://inferno.wodify.com/OnlineSalesPortal/Home.aspx" target="_blank" rel="noopener noreferrer">Book Now</a>
+                  </div>
+               </div>
+            </li>
+
+            <li>
+               <div className="left-container">
+                  <div className="day">Sunday</div>
+                  <div className="time santry">9:00am, 10:00am</div>
+                  <div className="time killester">9:00am, 10:00am</div>
                </div>
                <div className="right-container">
                   <div className="book-btn">
