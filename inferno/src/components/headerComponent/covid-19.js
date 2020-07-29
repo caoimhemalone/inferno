b57fdccc578ac9a604845c0847611e43e2572c3b
Caoimhe Personal <caoimhepersonal@Caoimhes-MacBook-Pro.local>
2020-07-29 19:49:18 +0100

    Covid message updated

--- inferno/src/components/headerComponent/covid-19.js
+++ inferno/src/components/headerComponent/covid-19.js
@@ -4,7 +4,7 @@ function Covid() {
   return (
     <section className="covid-section">
        <span>Covid-19 Update!</span>
-       <span>Our studios will be open again from June 29th. Please follow our <a href="https://www.instagram.com/inferno_fitness_dublin" target="_blank" rel="noopener noreferrer">Instagram</a> for more updates</span>
+       <span>Business back to normal! Keep an eye on our <a href="https://www.instagram.com/inferno_fitness_dublin" target="_blank" rel="noopener noreferrer">Instagram</a> for any timetable updates</span>
     </section>
   );
 }
